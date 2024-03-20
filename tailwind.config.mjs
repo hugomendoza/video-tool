736ca93df919dece0b666f2ed80047f2cc779097
Hugo <hugo_mendoza_s@outlook.com>
2024-03-20 11:47:22 -0500

    feat: initial configuration

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+  extend: {
+      colors: {
+        'primary': 'var(--primary-brand)',
+        'secondary': 'var(--secondary-brand)',
+        gray: {
+          100: 'var(--light-gray)'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
